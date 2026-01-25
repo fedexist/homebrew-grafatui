054ec17754aa4aa41ace37a12a5f2c3533f04b14
Federico D'Ambrosio <fedexist@gmail.com>
2026-01-25 23:37:07 +0100

    Add grafatui formula

---
+++ Formula/grafatui.rb
@@ -0,0 +1,36 @@
+class Grafatui < Formula
+  desc "A Grafana-like Terminal User Interface for Prometheus"
+  homepage "https://github.com/fedexist/grafatui"
+  version "0.1.3"
+
+  if OS.mac?
+    if Hardware::CPU.intel?
+      url "https://github.com/fedexist/grafatui/releases/download/v0.1.3/grafatui-x86_64-apple-darwin.tar.gz"
+      sha256 "REPLACE_WITH_SHA256"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/fedexist/grafatui/releases/download/v0.1.3/grafatui-aarch64-apple-darwin.tar.gz"
+      sha256 "REPLACE_WITH_SHA256"
+    end
+  elsif OS.linux?
+    if Hardware::CPU.intel?
+      url "https://github.com/fedexist/grafatui/releases/download/v0.1.3/grafatui-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "REPLACE_WITH_SHA256"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/fedexist/grafatui/releases/download/v0.1.3/grafatui-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "REPLACE_WITH_SHA256"
+    end
+  end
+
+  def install
+    bin.install "grafatui"
+    generate_completions_from_executable(bin/"grafatui", "completions")
+    
+    # Generate and install man page
+    system "#{bin}/grafatui", "man", ">", "grafatui.1"
+    man1.install "grafatui.1"
+  end
+
+  test do
+    system "#{bin}/grafatui", "--help"
+  end
+end
